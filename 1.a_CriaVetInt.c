51d513aea729bb76172b4f91103379e4c7917c47
Pedro Barbosa <pedrobarbosaocb@gmail.com>
2022-12-21 11:32:56 -0300

    feat: exercicio 1 item a

--- 1.a_CriaVetInt.c
+++ 1.a_CriaVetInt.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int CriaVetInt() {
 
-    printf("Hello World!");
+    int qtdElementosVet1;
+    int *vet1;
+
+    // recebe quantidade de elementos para o vetor 1
+    scanf("%d", &qtdElementosVet1);
+
+    // aloca uma espaços (qtdElementosVet) de memoria
+    vet1 = (int*) malloc (qtdElementosVet1 * sizeof(int));
+
+    printf("%p\n", &vet1);
+
+    free(vet1);
     return 0;
 }
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Essa eh a main\n");
     CriaVetInt();
     return 0;
 }
